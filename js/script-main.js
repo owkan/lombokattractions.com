d53f71b877aa05747de1783986d674078035f13e
okan <ocan.anonymous@gmail.com>
2024-05-24 22:45:01 +0800

    tambahan fitur slider untuk section 2 offer

--- js/script-main.js
+++ js/script-main.js
@@ -8,7 +8,7 @@ window.addEventListener('scroll', function () {
   if (scrollTop > lastScrollTop) {
     // Scroll ke atas
     if (scrollTop > header.offsetHeight) {
-      header.style.transform = 'translateY(-120px)';
+      header.style.transform = 'translateY(-70px)';
     } else {
       header.style.transform = 'translateY(0)';
     }
@@ -32,4 +32,58 @@ document.querySelector('#ellipsis-btn').addEventListener('click', () => {
 document.querySelector('.close-user-control').addEventListener('click', () => {
   userControl.classList.remove('user-controls-active');
 });
-// ------------------------------------------
+// -----------------------------------------------------------
+
+// Slider Carousel ---------------------
+// -------------------------------------
+document.addEventListener('DOMContentLoaded', function () {
+  const carousel = document.querySelector('.sub-container-a');
+  const cards = document.querySelectorAll('.card-a');
+  let currentIndex = 0;
+
+  function moveToNextCard() {
+    if (currentIndex < cards.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    updateCarousel();
+  }
+
+  function moveToPreviousCard() {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = cards.length - 1;
+    }
+    updateCarousel();
+  }
+
+  function updateCarousel() {
+    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+    const rightNav = document.getElementById('right-nav');
+    const leftNav = document.getElementById('left-nav');
+
+    if (currentIndex === cards.length - 1) {
+      rightNav.style.opacity = '0.5'; // Menurunkan opacity ketika di card terakhir
+      rightNav.style.transition = '0.3s'; // Menurunkan opacity ketika di card terakhir
+    } else {
+      rightNav.style.opacity = '1'; // Mengembalikan opacity normal ketika tidak di card terakhir
+    }
+
+    if (currentIndex === 0) {
+      leftNav.style.opacity = '0.5'; // Menurunkan opacity ketika di card pertama
+      leftNav.style.transition = '0.3s'; // Menurunkan opacity ketika di card pertama
+    } else {
+      leftNav.style.opacity = '1'; // Mengembalikan opacity normal ketika tidak di card pertama
+    }
+  }
+
+  document.getElementById('right-nav').addEventListener('click', function () {
+    moveToNextCard();
+  });
+  document.getElementById('left-nav').addEventListener('click', function () {
+    moveToPreviousCard();
+  });
+});
